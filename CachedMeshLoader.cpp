d1e02bafaa7ce8a91fdf2ad7e2f5c3bfb3425161
peterk <peterk@livingwork.com>
2023-10-25 17:08:21 -0400

    added LatLongSphere generator

--- CachedMeshLoader.cpp
+++ CachedMeshLoader.cpp
@@ -17,7 +17,9 @@ namespace fs = std::filesystem;
 struct VertexData {
     Vec3f pos;
     Vec3f normal;
-    Vec3f color;
+    glm::vec2 uv;
+//    float pad_;
+//    float pad2_;
 };
 
 #pragma pack(2)
@@ -26,6 +28,240 @@ struct TriangleIndices {
 };
 #pragma pack(0)
 
+#if 0
+//
+
+// items from https://danielsieger.com/blog/2021/01/03/generating-platonic-solids.html
+//
+// https://danielsieger.com/download/MyViewer_platonic_solids.cpp
+// Copyright 2011-2021 the Polygon Mesh Processing Library developers.
+// Distributed under a MIT-style license, see LICENSE.txt for details.
+//
+// https://www.pmp-library.org/
+//
+// repo:
+// git clone https://github.com/pmp-library/pmp-library.git
+
+void dual(SurfaceMesh& mesh)
+{
+  // the new dual mesh
+  SurfaceMesh tmp;
+
+  // a property to remember new vertices per face
+  auto fvertex = mesh.add_face_property<Vertex>("f:vertex");
+
+  // for each face add the centroid to the dual mesh
+  for (auto f : mesh.faces())
+    fvertex[f] = tmp.add_vertex(centroid(mesh, f));
+
+  // add new face for each vertex
+  for (auto v : mesh.vertices()) {
+    std::vector<Vertex> vertices;
+    for (auto f : mesh.faces(v))
+      vertices.push_back(fvertex[f]);
+
+    tmp.add_face(vertices);
+  }
+
+  // swap old and new meshes, don't copy properties
+  mesh.assign(tmp);
+}
+
+Point centroid(const SurfaceMesh& mesh, Face f)
+{
+  Point c(0, 0, 0);
+  Scalar n(0);
+  for (auto v : mesh.vertices(f)) {
+    c += mesh.position(v);
+    ++n;
+  }
+  c /= n;
+  return c;
+}
+
+#include "MyViewer.h"
+
+#include <imgui.h>
+
+using namespace pmp;
+
+SurfaceMesh tetrahedron()
+{
+    SurfaceMesh mesh;
+
+    // choose coordinates on the unit sphere
+    float a = 1.0f / 3.0f;
+    float b = sqrt(8.0f / 9.0f);
+    float c = sqrt(2.0f / 9.0f);
+    float d = sqrt(2.0f / 3.0f);
+
+    // add the 4 vertices
+    auto v0 = mesh.add_vertex(Point(0, 0, 1));
+    auto v1 = mesh.add_vertex(Point(-c, d, -a));
+    auto v2 = mesh.add_vertex(Point(-c, -d, -a));
+    auto v3 = mesh.add_vertex(Point(b, 0, -a));
+
+    // add the 4 faces
+    mesh.add_triangle(v0, v1, v2);
+    mesh.add_triangle(v0, v2, v3);
+    mesh.add_triangle(v0, v3, v1);
+    mesh.add_triangle(v3, v2, v1);
+
+    return mesh;
+}
+
+SurfaceMesh hexahedron()
+{
+    SurfaceMesh mesh;
+
+    // choose coordinates on the unit sphere
+    float a = 1.0f / sqrt(3.0f);
+
+    // add the 8 vertices
+    auto v0 = mesh.add_vertex(Point(-a, -a, -a));
+    auto v1 = mesh.add_vertex(Point(a, -a, -a));
+    auto v2 = mesh.add_vertex(Point(a, a, -a));
+    auto v3 = mesh.add_vertex(Point(-a, a, -a));
+    auto v4 = mesh.add_vertex(Point(-a, -a, a));
+    auto v5 = mesh.add_vertex(Point(a, -a, a));
+    auto v6 = mesh.add_vertex(Point(a, a, a));
+    auto v7 = mesh.add_vertex(Point(-a, a, a));
+
+    // add the 6 faces
+    mesh.add_quad(v3, v2, v1, v0);
+    mesh.add_quad(v2, v6, v5, v1);
+    mesh.add_quad(v5, v6, v7, v4);
+    mesh.add_quad(v0, v4, v7, v3);
+    mesh.add_quad(v3, v7, v6, v2);
+    mesh.add_quad(v1, v5, v4, v0);
+
+    return mesh;
+}
+
+Point centroid(const SurfaceMesh& mesh, Face f)
+{
+    Point c(0, 0, 0);
+    Scalar n(0);
+    for (auto v : mesh.vertices(f))
+    {
+        c += mesh.position(v);
+        ++n;
+    }
+    c /= n;
+    return c;
+}
+
+void dual(SurfaceMesh& mesh)
+{
+    // the new dual mesh
+    SurfaceMesh tmp;
+
+    // a property to remember new vertices per face
+    auto fvertex = mesh.add_face_property<Vertex>("f:vertex");
+
+    // for each face add the centroid to the dual mesh
+    for (auto f : mesh.faces())
+        fvertex[f] = tmp.add_vertex(centroid(mesh, f));
+
+    // add new face for each vertex
+    for (auto v : mesh.vertices())
+    {
+        std::vector<Vertex> vertices;
+        for (auto f : mesh.faces(v))
+            vertices.push_back(fvertex[f]);
+
+        tmp.add_face(vertices);
+    }
+
+    // swap old and new meshes, don't copy properties
+    mesh.assign(tmp);
+}
+
+SurfaceMesh octahedron()
+{
+    auto mesh = hexahedron();
+    dual(mesh);
+    return mesh;
+}
+
+void project_to_unit_sphere(SurfaceMesh& mesh)
+{
+    for (auto v : mesh.vertices())
+    {
+        auto p = mesh.position(v);
+        auto n = norm(p);
+        mesh.position(v) = (1.0 / n) * p;
+    }
+}
+
+SurfaceMesh octahedron2()
+{
+    auto mesh = hexahedron();
+    dual(mesh);
+    project_to_unit_sphere(mesh);
+    return mesh;
+}
+
+SurfaceMesh icosahedron()
+{
+    SurfaceMesh mesh;
+
+    float phi = (1.0f + sqrt(5.0f)) * 0.5f; // golden ratio
+    float a = 1.0f;
+    float b = 1.0f / phi;
+
+    // add vertices
+    auto v1 = mesh.add_vertex(Point(0, b, -a));
+    auto v2 = mesh.add_vertex(Point(b, a, 0));
+    auto v3 = mesh.add_vertex(Point(-b, a, 0));
+    auto v4 = mesh.add_vertex(Point(0, b, a));
+    auto v5 = mesh.add_vertex(Point(0, -b, a));
+    auto v6 = mesh.add_vertex(Point(-a, 0, b));
+    auto v7 = mesh.add_vertex(Point(0, -b, -a));
+    auto v8 = mesh.add_vertex(Point(a, 0, -b));
+    auto v9 = mesh.add_vertex(Point(a, 0, b));
+    auto v10 = mesh.add_vertex(Point(-a, 0, -b));
+    auto v11 = mesh.add_vertex(Point(b, -a, 0));
+    auto v12 = mesh.add_vertex(Point(-b, -a, 0));
+
+    project_to_unit_sphere(mesh);
+
+    // add triangles
+    mesh.add_triangle(v3, v2, v1);
+    mesh.add_triangle(v2, v3, v4);
+    mesh.add_triangle(v6, v5, v4);
+    mesh.add_triangle(v5, v9, v4);
+    mesh.add_triangle(v8, v7, v1);
+    mesh.add_triangle(v7, v10, v1);
+    mesh.add_triangle(v12, v11, v5);
+    mesh.add_triangle(v11, v12, v7);
+    mesh.add_triangle(v10, v6, v3);
+    mesh.add_triangle(v6, v10, v12);
+    mesh.add_triangle(v9, v8, v2);
+    mesh.add_triangle(v8, v9, v11);
+    mesh.add_triangle(v3, v6, v4);
+    mesh.add_triangle(v9, v2, v4);
+    mesh.add_triangle(v10, v3, v1);
+    mesh.add_triangle(v2, v8, v1);
+    mesh.add_triangle(v12, v10, v7);
+    mesh.add_triangle(v8, v11, v7);
+    mesh.add_triangle(v6, v12, v5);
+    mesh.add_triangle(v11, v9, v5);
+
+    return mesh;
+}
+
+SurfaceMesh dodecahedron()
+{
+    auto mesh = icosahedron();
+    dual(mesh);
+    project_to_unit_sphere(mesh);
+    return mesh;
+}
+
+#endif
+
+namespace {
 
 static void generateSimpleCubeMesh(std::vector<float>& pointData,
                              std::vector<uint16_t>& indexData, float extent)
@@ -128,13 +364,237 @@ static void generateSimpleCubeMesh(std::vector<float>& pointData,
     return;
 }
 
-static void generateConeMesh(uint32_t numSegments, std::vector<float>& pointData,
-                             std::vector<uint16_t>& indexData, float height, float radius)
+class TriangleMeshBuilder {
+
+    VertexData *verts;
+    VertexData *currVert;
+    int vertCount = 0;
+
+    TriangleIndices *indices;
+    int faceCount = 0;
+public:
+
+    TriangleMeshBuilder(VertexData *vbuf, TriangleIndices *ti)
+        : verts(vbuf)
+        , indices(ti)
+    {}
+
+    int addVertex(const Vec3f &point)  {
+        currVert = &verts[vertCount];
+        currVert->pos = point;
+        int ret = vertCount;
+        ++vertCount;
+        return(ret);
+    }
+    
+    void setNormal(const Vec3f &normal)  {
+        currVert->normal = normal;
+    }
+    
+    void setUV(const glm::vec2 &uv) {
+        currVert->uv = uv;
+    }
+    
+    void addTriangle(int i0, int i1, int i2) {
+        TriangleIndices &ti = indices[faceCount];
+        ++faceCount;
+        ti.indices[0] = i0;
+        ti.indices[1] = i1;
+        ti.indices[2] = i2;
+    }
+
+    // This projects all points from origin to the unit (radius) sphere.
+    // assuming the current state if a regular polyhedron
+    // with it's center at 0,0,0
+    
+    void projectToUnitSphere(bool setNormals) {
+        for(int i = 0; i < vertCount; ++i) {
+            Vec3f &v = verts[i].pos;
+            v = glm::normalize(v);
+            if(setNormals) {
+                verts[i].normal = v;
+            }
+        }
+    }
+};
+
+static void buildIcosahedron(TriangleMeshBuilder &mb) {
+    
+    float phi = (1.0f + sqrt(5.0f)) * 0.5f; // golden ratio
+    float a = 1.0f;
+    float b = 1.0f / phi;
+
+    // add vertices
+    auto v1 = mb.addVertex(Vec3f(0, b, -a));
+    auto v2 = mb.addVertex(Vec3f(b, a, 0));
+    auto v3 = mb.addVertex(Vec3f(-b, a, 0));
+    auto v4 = mb.addVertex(Vec3f(0, b, a));
+    auto v5 = mb.addVertex(Vec3f(0, -b, a));
+    auto v6 = mb.addVertex(Vec3f(-a, 0, b));
+    auto v7 = mb.addVertex(Vec3f(0, -b, -a));
+    auto v8 = mb.addVertex(Vec3f(a, 0, -b));
+    auto v9 = mb.addVertex(Vec3f(a, 0, b));
+    auto v10 = mb.addVertex(Vec3f(-a, 0, -b));
+    auto v11 = mb.addVertex(Vec3f(b, -a, 0));
+    auto v12 = mb.addVertex(Vec3f(-b, -a, 0));
+
+    // add triangles
+    mb.addTriangle(v3, v2, v1);
+    mb.addTriangle(v2, v3, v4);
+    mb.addTriangle(v6, v5, v4);
+    mb.addTriangle(v5, v9, v4);
+    mb.addTriangle(v8, v7, v1);
+    mb.addTriangle(v7, v10, v1);
+    mb.addTriangle(v12, v11, v5);
+    mb.addTriangle(v11, v12, v7);
+    mb.addTriangle(v10, v6, v3);
+    mb.addTriangle(v6, v10, v12);
+    mb.addTriangle(v9, v8, v2);
+    mb.addTriangle(v8, v9, v11);
+    mb.addTriangle(v3, v6, v4);
+    mb.addTriangle(v9, v2, v4);
+    mb.addTriangle(v10, v3, v1);
+    mb.addTriangle(v2, v8, v1);
+    mb.addTriangle(v12, v10, v7);
+    mb.addTriangle(v8, v11, v7);
+    mb.addTriangle(v6, v12, v5);
+    mb.addTriangle(v11, v9, v5);
+}
+
+static void generateLatLongSphere(std::vector<float>& pointData,
+                                std::vector<uint16_t>& indexData,
+                                int numRings, int numSegments)
 {
-    //CLEVER?
+    // TODO: mesh generation needs to be done in a separate thread structure.  Doing it here can result in the generation of multiple copies of the same mesh
+
+    int vertexCount = (numRings + 1) * (numSegments + 1);
+    int triCount = (2 * numSegments) + ((numRings - 2) * numSegments * 2) + 1;
 
     pointData.clear();
     indexData.clear();
+
+    const int floatsPerVert = (int)(sizeof(VertexData) / sizeof(float));
+
+    pointData.resize(vertexCount * floatsPerVert);
+    indexData.resize(triCount * 3);
+
+    TriangleMeshBuilder mb(
+        reinterpret_cast<VertexData*>(pointData.data())
+        , reinterpret_cast<TriangleIndices*>(indexData.data())
+    );
+
+    float ringDelta = (glm::pi<float>() / numRings);
+    float segmentDelta = (2 * glm::pi<float>() / numSegments);
+
+    // Generate sphere rings ..
+
+    for (int ring = 0; ring <= numRings; ++ring) {
+
+        float r0 = std::sin((float)ring * ringDelta); // Y
+        float y0 = std::cos((float)ring * ringDelta); // polar from top down
+
+        // segment group for current ring
+
+        for (int seg = 0; seg <= numSegments; ++seg) {
+            
+            float x0 = r0 * std::sin((float)seg * segmentDelta);
+            float z0 = r0 * std::cos((float)seg * segmentDelta);
+            
+            auto vIndex = mb.addVertex(Vec3f(x0,y0,z0));
+            mb.setUV(glm::vec2((float) seg / (float) numRings, (float) ring / (float) numRings));
+            
+            if(ring > 0) { // top
+                
+                mb.addTriangle(vIndex,
+                               vIndex - numSegments,
+                               vIndex - (numSegments + 1)
+                               );
+                
+                if(ring < numRings)  { // bottom or second in quad
+                    
+                    mb.addTriangle(vIndex,
+                                   vIndex - (numSegments + 1),
+                                   vIndex - 1
+                                   );
+                }
+            }
+        }
+    }
+    mb.projectToUnitSphere(true);
+}
+
+static void generateIcosoSphere(std::vector<float>& pointData,
+                                std::vector<uint16_t>& indexData,
+                                int // subdivisions = 1
+                                )
+{
+    pointData.clear();
+    indexData.clear();
+
+    int vCount = 12;   // 12 vertices
+    int triCount = 20; // 20 faces
+
+    // for now only one subdivision but should be scalable.
+
+//    vcount += triCount;
+//    triCount *= 3;  // each subdivide creates 3 triangles.
+
+    const int floatsPerVert = (int)(sizeof(VertexData) / sizeof(float));
+
+    pointData.resize(vCount * floatsPerVert);
+    indexData.resize(triCount * 3);
+
+    TriangleMeshBuilder mb(
+        reinterpret_cast<VertexData*>(pointData.data())
+        , reinterpret_cast<TriangleIndices*>(indexData.data())
+    );
+
+    buildIcosahedron(mb);
+    mb.projectToUnitSphere(true);
+}
+
+// https://prideout.net/blog/octasphere/
+
+static void generateOctoSphere(std::vector<float>& pointData,
+                                std::vector<uint16_t>& indexData,
+                                int // subdivisions = 1
+                                )
+{
+    pointData.clear();
+    indexData.clear();
+
+    int vCount = 12;   // 12 vertices
+    int triCount = 20; // 20 faces
+
+        // for now only one subdivision but should be scalable.
+
+//    vcount += triCount;
+//    triCount *= 3;  // each subdivide creates 3 triangles.
+
+    const int floatsPerVert = (int)(sizeof(VertexData) / sizeof(float));
+
+    pointData.resize(vCount * floatsPerVert);
+    indexData.resize(triCount * 3);
+
+    TriangleMeshBuilder mb(
+        reinterpret_cast<VertexData*>(pointData.data())
+        , reinterpret_cast<TriangleIndices*>(indexData.data())
+    );
+
+    buildIcosahedron(mb);
+    mb.projectToUnitSphere(true);
+    
+    
+}
+
+
+
+
+static void generateConeMesh(uint32_t numSegments, std::vector<float>& pointData,
+                                                                                std::vector<uint16_t>& indexData, float height, float radius)
+{
+    //CLEVER?
+
 
 	uint32_t vertexCount = numSegments * 3 + 1; // top and bottom vertices for sides, bottom and center vertices for bottom cap
 	uint32_t indexCount = 2 * numSegments; // triangles for sides and triangles for bottom cap
@@ -237,7 +697,7 @@ static void generateConeMesh(uint32_t numSegments, std::vector<float>& pointData
 }
 
 
-
+} // end anonymous namespace
 
 bool
 CachedMeshLoader::loadGeometry(const fs::path& path, std::vector<float>& pointData,
@@ -252,6 +712,22 @@ CachedMeshLoader::loadGeometry(const fs::path& path, std::vector<float>& pointDa
     if(path == "cube") {
         generateSimpleCubeMesh( pointData,
                                 indexData, 1.0f);
+        return(true);
+    }
+    if(path == "sphere") {
+        generateLatLongSphere( pointData,
+                             indexData, 12, 24);
+        return(true);
+
+    }
+    if(path == "octoSphere") {
+        generateOctoSphere( pointData,
+                            indexData, 1);
+        return(true);
+    }
+    if(path == "icosoSphere") {
+        generateIcosoSphere( pointData,
+                             indexData, 1);
         return(true);
     }
 
